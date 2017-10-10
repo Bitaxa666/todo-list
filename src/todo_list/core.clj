f76f00ff068059d6c6f77132f05ad806641cac08
SUGIMURA Takashi <sugimura.takashi@tis.co.jp>
2017-10-10 10:18:22 +0900

    create a handler function

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -1,11 +1,21 @@
 (ns todo-list.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn welcome
+  "A ring handler to process all requests for the web server.  If a request is for something other than then an error message is returned"
+  [request]
+  (if (= "/" (:uri request))
+    {:status 200
+     :body "<h1>Hello, Clojure World</h1>
+            <p>Welcome to your first Clojure app.</p>"
+     :headers {}}
+    {:status 404
+     :body "<h1>This is not the page you are looking for</h1>
+            <p>Sorry, the page you requested was not found!</p>"
+     :headers {}}))
+
 (defn -main
   "A very simple web server using Ring & Jetty"
   [port-number]
-  (jetty/run-jetty
-   (fn [request] {:status 200
-                  :body "<h1>Hello, Clojure World</h1>  <p>Welcome to your first Clojure app.  This message is returned regardless of the request, sorry</p>"
-                  :headers {}})
-   {:port (Integer. port-number)}))
+  (jetty/run-jetty welcome
+                   {:port (Integer. port-number)}))

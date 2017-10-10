b087e623e4e44c7e0fa7ad0f0457b4bbc79e612c
SUGIMURA Takashi <sugimura.takashi@tis.co.jp>
2017-10-10 14:06:40 +0900

    separate handlers

---
+++ src/todo_list/handlers/base.clj
@@ -0,0 +1,67 @@
+(ns todo-list.handlers.base
+  (:require
+            [compojure.route :refer [not-found]]
+            [compojure.core :refer [defroutes GET]]
+            [ring.handler.dump :refer [handle-dump]]
+            [hiccup.core :refer :all]
+            [hiccup.page :refer :all]))
+
+(defn welcome
+  "A ring handler to respond with a simple welcome message"
+  [request]
+  (html [:h1 "Hello, Clojure World"]
+        [:p "Welcome to your first Clojure app, I now update automatically"]))
+
+(defn goodbye
+  "A song to wish you goodbye"
+  [request]
+  (html5 {:lang "en"}
+         [:head (include-js "myscript.js") (include-css "mystyle.css")]
+         [:body
+          [:div [:h1 {:class "info"} "Walking back to happiness"]]
+          [:div [:p "Walking back to happiness with you"]]
+          [:div [:p "Said, Farewell to loneliness I knew"]]
+          [:div [:p "Laid aside foolish pride"]]
+          [:div [:p "Learnt the truth from tears I cried"]]]))
+
+(defn about
+  "Information about the website developer"
+  [request]
+  {:status 200
+   :body "I am an awesome Clojure developer, well getting there..."
+   :headers {}})
+
+(defn hello
+  "A simple personalised greeting showing the use of variable path elements"
+  [request]
+  (let [name (get-in request [:route-params :name])]
+    {:status 200
+     :body (str "Hello " name ".  I got your name from the web URL")
+     :headers {}}))
+
+(def operands {"+" + "-" - "*" * ":" /})
+
+(defn calculator
+  "A very simple calculator that can add, divide, subtract and multiply.  This is done through the magic of variable path elements."
+  [request]
+  (let [a  (Integer. (get-in request [:route-params :a]))
+        b  (Integer. (get-in request [:route-params :b]))
+        op (get-in request [:route-params :op])
+        f  (get operands op)]
+    (if f
+      {:status 200
+       :body (str "Calculated result: " (f a b))
+       :headers {}}
+      {:status 404
+       :body "Sorry, unknown operator.  I only recognise + - * : (: is for division)"
+       :headers {}})))
+
+(defroutes app
+  (GET "/" [] welcome)
+  (GET "/goodbye" [] goodbye)
+  (GET "/about" [] about)
+  (GET "/request-info" [] handle-dump)
+  (GET "/hello/:name" [] hello)
+  (GET "/calculator/:op/:a/:b" [] calculator)
+  (not-found "<h1>This is not the page you are looking for</h1>
+              <p>Sorry, the page you requested was not found!</p>"))
